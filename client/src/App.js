3a87bb410294a0d40851d79ae698cb6726f0a033
secret0724 <sultannafis1324@gmail.com>
2025-11-07 04:01:14 +0700

    Add client folder as normal folder

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome back to the TANIBUDAYA website, create by anak nya siskae, heny, aib and murtaba
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
